b535b3b82bc4b9efb8fc519c25281a29f1a5c5e3
Alina-ps <alinaserg2014@gmail.com>
2024-08-19 20:20:55 +0200

    minor correction

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,26 +1,20 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { selectIsLoggedIn, selectUser } from '../../redux/auth/selectors';
+import { selectUser } from '../../redux/auth/selectors';
 import { logoutThunk } from '../../redux/auth/operations';
 import s from './UserMenu.module.css';
-import { Navigate } from 'react-router-dom';
 
 const UserMenu = () => {
   const dispatch = useDispatch();
   const user = useSelector(selectUser);
-  const isLoggedIn = useSelector(selectIsLoggedIn);
 
-  if (!isLoggedIn) {
-    return <Navigate to="/" />;
-  }
+  const handleLogOut = () => {
+    dispatch(logoutThunk());
+  };
 
   return (
     <div className={s.wrapper}>
       <p className={s.text}>{user.name}</p>
-      <button
-        className={s.btn}
-        type="submit"
-        onClick={() => dispatch(logoutThunk())}
-      >
+      <button className={s.btn} type="submit" onClick={handleLogOut}>
         Logout
       </button>
     </div>
